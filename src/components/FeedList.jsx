508f7dcfc80463cebe8725cebea4af87e9254302
rende11 <artamonovn@gmail.com>
2018-05-09 16:30:33 +0300

    add validation

--- src/components/FeedList.jsx
+++ src/components/FeedList.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Col } from 'react-bootstrap';
+import uuid from 'uuid/v1';
 
 export default class FeedList extends Component {
 
   render() {
     return (
-      <ListGroup>
-        {this.props.links.map(link => <ListGroupItem>{link}</ListGroupItem>)}
-      </ListGroup>);
+      <Col md={8} mdOffset={2}>
+        <ListGroup>
+          {this.props.links.map(link => <ListGroupItem key={uuid()} href="#">{link}</ListGroupItem>)}
+        </ListGroup>
+      </Col>);
   }
 }
--- src/components/RssForm.jsx
+++ src/components/RssForm.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
+import isEmail from 'validator/lib/isEmail';
+import cn from 'classnames';
 
 import FeedList from './FeedList';
 
 export default class RssForm extends Component {
 
-  state = { rssLink: '' };
+  state = { rssLink: '', formState: 'neutral' };
 
   handleOnChange = (e) => {
     const value = e.target.value;
@@ -14,16 +16,33 @@ export default class RssForm extends Component {
 
   handleOnSubmit = (e) => {
     e.preventDefault();
+    this.setValidationForm();
     this.props.onSubmit(this.state.rssLink);
     this.setState({ rssLink: '' })
   }
 
+  setValidationForm = () => {
+    const value = this.state.rssLink;
+    const result =  isEmail(value) ? 'valid' : 'invalid';
+    this.setState({ formState: result });
+  }
+
+
   render() {
+    const validatorClass = cn({
+      "has-error": this.state.formState === 'invalid',
+      "has-success": this.state.formState === 'valid',
+      "": this.state.formState === 'neutral',
+    });
+
     return  <Jumbotron>
       <h1> Hello RSS </h1>
       <form onSubmit={this.handleOnSubmit}>
-        <input type="text" name="rssLink" onChange={this.handleOnChange} placeholder="input link with rss" value={this.state.rssLink}/>
-        <input type="submit" value="submit" />
+          <div className={validatorClass}>
+            <FormControl type="text" name="rssLink" onChange={this.handleOnChange} placeholder="input link with rss" value={this.state.rssLink}/>
+          </div>
+          <HelpBlock>Enter a valid RSS URL</HelpBlock>
+        <Button type="submit" bsStyle="info">Submit</Button>
       </form>
     </Jumbotron>
   }
